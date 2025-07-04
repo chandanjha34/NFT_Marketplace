903932b72b81cad61287c8eb070bfd6a931b0903
Chandan Jha <chandanjha34>
2025-07-05 01:04:10 +0530

    done

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -25,6 +25,7 @@ export default function Login(){
         Router.push('/');                  
     } catch (error) {
             toast('error');
+            console.log(error);
         }
     }
 
